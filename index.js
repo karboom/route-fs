73428cb40f4b0c4a6734f53c33086d6507442bce
karboom <mail@karboom.me>
2015-05-14 13:55:41 +0800

    Add route handle and test

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  */
 
 var fs = require('fs');
+var Route = require('route-parser');
 
 function Binder (config) {
     this.root = config.root;
@@ -31,17 +32,18 @@ Binder.prototype.list_file = function () {
 
 Binder.prototype.parse_uri = function () {
     var list = this.list_file(),
-        uris = [];
+        uris = [],
+        self = this;
 
     list.sort(function (a, b) {
         return a.split('/').length < b.split('/').length;
     });
 
     list.forEach(function (file) {
-        var uri_obj = {path:file, uri: file.replace('.js','').concat('/:id')};
+        var uri_obj = {path:file, uri: file.replace('.js','').concat('/:id').replace(self.root,'')};
 
         for (var i in list) {
-            var sub = list[i].replace('.js','');
+            var sub = list[i].replace('.js','').replace(self.root, '');
             if (list[i] != file && -1 != file.indexOf(sub)) {
                 uri_obj.uri = uri_obj.uri.replace(sub, sub.replace(/\/(\w+)$/, "/:$1"));
             }
@@ -53,6 +55,29 @@ Binder.prototype.parse_uri = function () {
     return uris;
 };
 
+Binder.prototype.handle = function () {
+    var self = this;
 
+    return function (req, res, next) {
+        var routes = self.parse_uri(), i, route, params;
+        //error master
+
+       for(i in routes) {
+           route = new Route(routes[i].uri);
+
+           if ((params = route.match(req.url))) {
+               req.params = params;
+
+               var func = require(routes[i].path);
+               func[req.method.toLowerCase()](req, res, next);
+
+               return;
+           }
+       }
+
+        res.status(404);
+        res.send({asd:123});
+    };
+};
 
 module.exports = Binder;
---
+++ test/index.js
@@ -0,0 +1,79 @@
+require('should');
+var restify = require('restify');
+var request = require('request');
+var main, base, server;
+
+describe("Binder", function () {
+    before(function () {
+        base = "/home/karboom/projects/javascript/route-fs/test/tmp";
+        main = new (require('../index.js'))({root:base});
+        server = restify.createServer();
+        server.use(restify.queryParser());
+        server.use(restify.bodyParser());
+        server.get(/.+/,main.handle());
+        server.listen(3200, function (err) {
+            if (err) console.log(err);
+        });
+        server.on('uncaughtException', function (req,res,route,err) {
+            console.log(err.stack);
+        });
+
+    });
+
+    describe("#list_file", function () {
+        it("should return all files", function () {
+            var list = main.list_file();
+
+            list.should.containEql(base + '/public/person.js');
+            list.should.containEql(base + '/public/unit/computer.js');
+        });
+    });
+
+    describe("#parse_uri", function () {
+
+        it("should found the sub uri", function () {
+            var uris = main.parse_uri();
+
+            uris.should.containEql({path:base + '/public/person/pet.js', uri:  '/public/:person/pet/:id'});
+            uris.should.containEql({path:base + '/public/person/pet/dog.js', uri: '/public/:person/:pet/dog/:id'});
+            uris.should.containEql({path:base + '/public/person.js', uri: '/public/person/:id'});
+        });
+
+    });
+
+    describe("#handle", function () {
+        it("should 404", function () {
+            request.get("http://localhost:3200/test").on('response', function (res) {
+                res.statusCode.should.equal(404);
+            });
+        });
+
+        it("should parse id", function (done) {
+            request.get("http://localhost:3200/public/person/85757").on('response', function (res) {
+                res.setEncoding('utf8');
+                res.on("data", function (data) {
+                    JSON.parse(data).should.eql({id: "85757"});
+                    done();
+                });
+            });
+        });
+
+        it("should parse every param", function (done) {
+
+            request.get("http://localhost:3200/public/85757/1/dog/2").on('response', function (res) {
+                res.setEncoding('utf8');
+                res.on('data', function (data) {
+                    JSON.parse(data).should.eql({person:'85757',pet:'1',id:'2'});
+                    done();
+                });
+            });
+        });
+    });
+
+    describe("#bind", function () {
+    });
+
+    after(function () {
+        
+    });
+});
